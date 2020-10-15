8fed1c273708c60303bf51692b9e352d0655b95e
Diego Osorio <diegoso-0989@hotmail.com>
2020-10-14 20:56:49 -0500

    Iniciando proyecto, conexón a base de datos exitosa

---
+++ src/main/java/com/platzi/mensajes_app/Conexion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platzi.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Diego OSorio
+ */
+public class Conexion {
+    public Connection get_connection(){
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
+            if(connection!= null){
+                System.out.println("Conexión exitosa");
+            }
+        }catch(SQLException e){
+            System.out.println(e);        
+        }
+        return connection;
+    }
+    
+}
