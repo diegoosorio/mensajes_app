b7eb3a8ccd1c0bf48990bb7f0057c1197ccddf7e
Diego Osorio <diegoso-0989@hotmail.com>
2020-10-19 09:23:35 -0500

    Implementación de operaciones CRUD

--- src/main/java/com/platzi/mensajes_app/Inicio.java
+++ src/main/java/com/platzi/mensajes_app/Inicio.java
@@ -6,6 +6,7 @@
 package com.platzi.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -13,11 +14,39 @@ import java.sql.Connection;
  */
 public class Inicio {
     public static void main(String[] args) {
-        Conexion conexion = new Conexion();
-        try(Connection cnx=conexion.get_connection()){
-            
-        }catch(Exception e){
-            System.out.println(e);
-        }
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int opcion = 0;
+        
+        do {
+            System.out.println("-------------------");
+            System.out.println(" Aplicación de mensajes");
+            System.out.println("    1. Crear un mensaje");
+            System.out.println("    2. Listar mensajes");
+            System.out.println("    3. Eliminar mensajes");
+            System.out.println("    4. Editar mensaje");
+            System.out.println("    5. Salir");
+            System.out.println("");
+            System.out.println("Opción: ");
+            //Leer opicion del usuario
+            opcion = sc.nextInt();
+            switch(opcion){
+                case 1: 
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensajes();
+                    break;
+                case 3: 
+                    MensajesService.borrarMensaje();
+                    break;
+                case 4:
+                    MensajesService.editarMensaje();
+                    break;
+                default: 
+                    break;                    
+            }
+        }while(opcion != 5);
     }
 }
